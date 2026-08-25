1982c517acc359d794c258a78c3298b16ea837f7
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2026-08-24 22:16:32 -0300

    chore: add biomejs as linter and formatter

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
+import cors from 'cors'
 import express from 'express'
-import cors from 'cors';
 
 const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/health', ( request, response ) => {
-  return response.status(200).json({message: "OK"})
+app.get('/health', (request, response) => {
+  return response.status(200).json({ message: 'OK' })
 })
 
 export { app }
